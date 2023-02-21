37a791604ffc03e5cea30bd10969ed94a982167f
Albert Tang <tangalbert919@yahoo.com>
2023-02-21 11:00:11 -0600

    sm6375-common: Import task_profiles.json
    Change-Id: I922e83abeb47706303f2b83698ea07c09a65b069

--- common.mk
+++ common.mk
@@ -212,6 +212,10 @@ PRODUCT_PACKAGES += \
     MotoActions \
     MotoCommonOverlay
 
+# Perf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
