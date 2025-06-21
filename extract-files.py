7af698930546b3ec9c9712320b8408983c0063d8
basamaryan <basam.aryan@gmail.com>
2025-06-21 12:59:43 -0400

    sm6375-common: Shim libwfdservice with libaudioclient_shim
    Change-Id: Idd6603ad16926226f22423a2bba2075d430015d5

--- extract-files.py
+++ extract-files.py
@@ -59,6 +59,7 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libbinder_shim.so')
         .add_needed('libinput_shim.so'),
     ('system_ext/lib/libwfdservice.so', 'system_ext/lib64/libwfdservice.so'): blob_fixup()
+        .add_needed('libaudioclient_shim.so')
         .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
     ('vendor/bin/init.kernel.post_boot-blair.sh', 'vendor/bin/init.kernel.post_boot-holi.sh'): blob_fixup()
         .regex_replace('ro.boot.using_zram_from_fstab', 'ro.vendor.zram.swapon'),
